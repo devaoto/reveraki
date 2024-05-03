af26c6ea7ef9b74f3eeabe2fcb66132b74fb0e0d
Mohtasim Alam <94981761+codeblitz97@users.noreply.github.com>
2024-05-04 00:44:42 +0600

    Create trending page

---
+++ src/app/trending/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { getTrendingAnime } from '@/functions/clientRequests';
+import { ConsumetAnimePage, ExtendedAnimePage } from '@/types/consumet';
+import { useEffect, useState } from 'react';
+import { Card } from './Card';
+import { Pagination } from '@nextui-org/react';
+
+export default function Trending() {
+  const [page, setPage] = useState(1);
+  const [trending, setTrending] = useState<ExtendedAnimePage | null>(null);
+
+  useEffect(() => {
+    fetchTrendingAnime(page);
+  }, [page]);
+
+  const fetchTrendingAnime = async (currentPage: number) => {
+    const response = await getTrendingAnime(currentPage, 46);
+    setTrending(response!);
+  };
+
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  return (
+    <div className="overflow-x-hidden">
+      <div className="grid grid-cols-2 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6">
+        {trending?.results.map((a) => {
+          return <Card key={a.id} anime={a} />;
+        })}
+      </div>
+      <div className="mt-10">
+        {trending && (
+          <Pagination
+            total={trending.totalPages}
+            initialPage={page}
+            onChange={handlePageChange}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
