5c59d821aa6a678a8fe27c6c349085db720fc4b4
Mohtasim Alam <94981761+codeblitz97@users.noreply.github.com>
2024-05-04 18:34:19 +0600

    Redesign EpisodesList component

--- src/components/EpisodesList.tsx
+++ src/components/EpisodesList.tsx
@@ -14,44 +14,45 @@ export const EpisodesList = ({
 }) => {
   return (
     <div className="gap-2 overflow-y-auto overflow-x-hidden">
-      <div className="max-h-96">
-        {episodes.map((e) => {
-          return (
-            <Link key={e.id} href={`/watch/${id}/${e.number}`}>
+      <div className="max-h-96 flex flex-col gap-4">
+        {episodes.map((episode) => (
+          <Link key={episode.id} href={`/watch/${id}/${episode.number}`}>
+            <motion.div
+              className="cursor-pointer bg-base-200"
+              whileHover={{ scale: 1.02 }}
+            >
               <motion.div
-                className="cursor-pointer bg-base-200"
-                whileHover={{ scale: 1.02 }}
+                className="flex gap-4"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5 }}
               >
-                <motion.div
-                  className="flex gap-4"
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 0.5 }}
-                >
-                  {e.img ? (
-                    <div className="flex flex-col gap-4 sm:flex-row">
-                      <div className="">
-                        <Image
-                          src={e.img!}
-                          alt={e.title}
-                          width={200}
-                          height={150}
-                          className="object-cover min-h-[150px] sm:min-w-[100px] sm:max-w-[200px] md:min-w-[150px] md:max-w-[250px] lg:min-w-[200px] lg:max-w-[300px] xl:min-w-[250px] xl:max-w-[350px]"
-                        />
-                      </div>
-                      <div className="flex flex-col">
-                        <h1>
-                          {e.number} - {e.title}
-                        </h1>
-                        <p className="text-xs">{e.description}</p>
-                      </div>
+                {episode.img && (
+                  <div className="flex flex-col gap-4 sm:flex-row">
+                    <div className="">
+                      <Image
+                        src={episode.img}
+                        alt={episode.title}
+                        width={1500}
+                        height={1500}
+                        className="object-cover min-h-[200px] sm:min-w-[100px] sm:max-w-[200px] md:min-w-[150px] md:max-w-[250px] lg:min-w-[200px] lg:max-w-[300px] xl:min-w-[250px] xl:max-w-[350px]"
+                      />
                     </div>
-                  ) : null}
-                </motion.div>
+                    <div className="flex flex-col">
+                      <h1>
+                        {episode.title.startsWith('Episode')
+                          ? ''
+                          : `${episode.number} -`}{' '}
+                        {episode.title}
+                      </h1>
+                      <p className="text-xs">{episode.description}</p>
+                    </div>
+                  </div>
+                )}
               </motion.div>
-            </Link>
-          );
-        })}
+            </motion.div>
+          </Link>
+        ))}
       </div>
     </div>
   );
