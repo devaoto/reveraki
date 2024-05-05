5f85239d9e39b16b9a940e5818694b05e545ca68
Mohtasim Alam <94981761+codeblitz97@users.noreply.github.com>
2024-05-05 11:43:53 +0600

    Create DMCA page

---
+++ src/app/dmca/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { Breadcrumbs, BreadcrumbItem, Link } from '@nextui-org/react';
+
+export default function DMCA() {
+  return (
+    <div className="mx-auto px-4 py-8">
+      <Breadcrumbs isDisabled size="lg">
+        <BreadcrumbItem>Home</BreadcrumbItem>
+        <BreadcrumbItem>DMCA</BreadcrumbItem>
+      </Breadcrumbs>
+
+      <h1 className="text-4xl font-bold mt-4 mb-2">DMCA Note</h1>
+      <p className="text-pink-200 text-2xl">
+        ReverAki operates independently and is not formally associated with nor
+        endorsed by any of the anime studios responsible for the creation of the
+        anime featured on this platform. Our website serves solely as a user
+        interface, facilitating access to self-hosted files sourced from various
+        third-party providers across the internet. It&apos;s important to note
+        that ReverAki never initiates downloads of video content from these
+        providers. Instead, links are provided in response to user requests,
+        thereby absolving the platform from any potential DMCA compliance
+        issues.
+      </p>
+    </div>
+  );
+}
