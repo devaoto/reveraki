1c3247aace2f2d109c17cf4e6da5187945387096
Mohtasim Alam <94981761+codeblitz97@users.noreply.github.com>
2024-05-05 18:16:29 +0600

    Add privacy policy page

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { Breadcrumbs, BreadcrumbItem } from '@nextui-org/react';
+
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <Breadcrumbs isDisabled size="lg">
+        <BreadcrumbItem>Home</BreadcrumbItem>
+        <BreadcrumbItem>Privacy Policy</BreadcrumbItem>
+      </Breadcrumbs>
+
+      <h1 className="mb-4 text-3xl font-semibold">Privacy Policy</h1>
+      <p className="mb-4 text-lg">
+        At Reveraki, we take your privacy seriously. We are committed to
+        protecting the privacy of our users and ensuring that all personal
+        information provided to us remains confidential.
+      </p>
+      <p className="mb-4 text-lg">
+        Reveraki does not collect any personal information from its users. We do
+        not use cookies or any other tracking technologies. We do not store or
+        share any data with third parties.
+      </p>
+      <p className="mb-4 text-lg">
+        Our app is designed to respect your privacy and anonymity. You can use
+        Reveraki with the assurance that your personal information will never be
+        compromised.
+      </p>
+      <p className="mb-4 text-lg">
+        If you have any questions or concerns about our privacy policy, please
+        feel free to contact us at privacy@reveraki.com.
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
