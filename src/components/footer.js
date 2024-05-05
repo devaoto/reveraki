9c115ef12500a260aba3430d64de44b9204562af
Sunil Gupta <sunilk98850@gmail.com>
2024-05-05 11:41:15 +0530

    Added footer code implemetation pending.

---
+++ src/components/footer.js
@@ -0,0 +1,103 @@
+"use client"
+import React from 'react'
+import Link from 'next/link'
+import { useTitle } from '@/components/player/components/title';  //website title import here.
+
+function Footer() {
+    const animetitle = useStore(useTitle, (state) => state.animetitle);
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth();
+
+    const handleToggle = () => {
+        if (animetitle === 'english') {
+            useTitle.setState({ animetitle: 'romaji' })
+        } else {
+            useTitle.setState({ animetitle: 'english' })
+        }
+    };
+
+    function getSeason(month) {
+        if (month === 12 || month === 1 || month === 2) {
+            return 'WINTER';
+        } else if (month === 3 || month === 4 || month === 5) {
+            return 'SPRING';
+        } else if (month === 6 || month === 7 || month === 8) {
+            return 'SUMMER';
+        } else {
+            return 'FALL';
+        }
+    }
+
+    const format = ['WINTER', 'SPRING', 'SUMMER', 'FALL'];
+
+    function nextSeason(currentSeason) {
+        const currentSeasonIndex = format.indexOf(currentSeason);
+        const nextSeasonIndex = (currentSeasonIndex + 1) % format.length;
+        return format[nextSeasonIndex];
+    }
+
+    return (
+        <div>
+            <footer className="bg-[#151518] mt-10">
+                <div className="mx-auto w-full lg:max-w-[85%] p-4 py-6 lg:pt-8 lg:pb-3">
+                    <div className="lg:flex lg:justify-between">
+                        <div className="mb-6 lg:mb-0">
+                            <Link href="/" className="flex items-center w-fit">
+                                <p className={`reveraki self-center text-3xl font-medium whitespace-nowrap dark:text-white`}>REVERAKI</p>
+                            </Link>
+                            <p className="font-karla lg:text-[0.8rem] text-[0.7rem] text-[#ffffffb2] lg:w-[520px]">
+                                This site does not store any files on our server, we are linked
+                                to the media which is hosted on 3rd party services.
+                            </p>
+                        </div>
+                                <ul className="font-semibold flex flex-col gap-2 lg:text-[0.85rem] text-[0.7rem] text-[#ffffffb2]">
+                                    <li>
+                                        <Link href="/dmca" className="hover:text-white"> DMCA</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="https://ko-fi.com/" target='_blank' className="hover:text-white !font-semibold !text-[0.8rem]">Donate</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='bg-tersier border-t border-white/5 mt-2'></div>
+                <div className="mx-auto w-full lg:max-w-[83%] lg:flex lg:items-center lg:justify-between lg:text-[0.8rem] text-[0.7rem] text-[#ffffffb2] py-3">
+                    <span className="sm:text-center ms-5 lg:ms-0">© {year} <Link href="/" className="hover:text-white">REVERAKI™</Link> | Made by <span className="font-bold" >codeblitz97</span>
+                    </span>
+                    <div className="flex mt-4 lg:justify-center lg:mt-0">
+                        <Link href="https://github.com/codeblitz97/reveraki" target="_blank" className=" hover:text-gray-900 dark:hover:text-white ms-5 lg:ms-0">
+                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path fillRule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clipRule="evenodd" />
+                            </svg>
+                            <span className="sr-only">GitHub account</span>
+                        </Link>
+                        <Link href="https://discord.gg/AKMfgTnd" target="_blank" className=" hover:text-gray-900 dark:hover:text-white ms-5">
+                            <svg className="w-[22px] h-[22px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
+                                <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
+                            </svg>
+                            <span className="sr-only">Discord community</span>
+                        </Link>
+                        <div className="flex items-center ml-5">
+                            <label className="relative cursor-pointer">
+                                {animetitle && (
+                                    <input
+                                        type="checkbox"
+                                        className="sr-only peer"
+                                        checked={animetitle === 'english'}
+                                        onChange={handleToggle}
+                                    />
+                                )}
+                                <div className="w-[40px] text-xs h-4 flex items-center bg-[#EAEEFB] rounded-full  peer-checked:text-[#18181b] text-[black] font-bold after:flex after:items-center after:justify-center peer after:content-['JP'] peer-checked:after:content-['EN'] peer-checked:after:translate-x-3/4 after:absolute peer-checked:after:border-white after:bg-white after:border after:border-gray-300 after:rounded-full after:h-6 after:w-6 after:transition-all peer-checked:bg-[#EAEEFB]">
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
